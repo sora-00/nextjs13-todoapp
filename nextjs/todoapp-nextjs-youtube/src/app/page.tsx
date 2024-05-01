30dbd979c92a58fd741b52fad406ac54e136903e
sora-00 <sora.umiushi@gmail.com>
2024-05-02 08:03:18 +0900

    nextjsはじめてのコミット

---
+++ nextjs/todoapp-nextjs-youtube/src/app/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import AddTask from "./components/AddTask";
+import Todolist from "./components/TodoList";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
+      <h1
+        className="text-4xl font-bold text-gray-700 -mt-32
+      "
+      >
+        Nextjs 13 Todo App
+      </h1>
+      <div className="w-full max-w-xl mt-5">
+        <div
+          className="w-full px-8 py-6 bg-white shadow-md rounded-lg
+        "
+        >
+          <AddTask />
+          <Todolist />
+        </div>
+      </div>
+    </main>
+  );
+}
