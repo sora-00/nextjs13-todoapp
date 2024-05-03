17d33f4f1248ced2a4a254f998aae88aa1ba2688
sora-00 <sora.umiushi@gmail.com>
2024-05-03 15:32:50 +0900

    tsks

--- nextjs/todoapp-nextjs-youtube/src/app/pages/api/api.ts
+++ nextjs/todoapp-nextjs-youtube/src/app/pages/api/api.ts
@@ -1,7 +1,7 @@
 import { Todo } from "./types";
 
 export const getAllTodos = async (): Promise<Todo[]> => {
-  const res = await fetch("http://localhost:3001/tsks", {
+  const res = await fetch("http://localhost:3001/tasks", {
     cache: "no-store", //SSR or CSR
   });
   const todos = res.json();
