b387aaa728085cda239b5f201b746e890f41dde7
sora-00 <sora.umiushi@gmail.com>
2024-05-07 01:58:05 +0900

    edit中のif文

--- nextjs/todoapp-nextjs-youtube/src/app/components/Todo.tsx
+++ nextjs/todoapp-nextjs-youtube/src/app/components/Todo.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Task } from "../pages/api/types";
 
 interface TodoProps {
@@ -6,14 +8,23 @@ interface TodoProps {
 }
 
 const Todo = ({ todo }: TodoProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = async () => {
+    setIsEditing(true);
+  };
+
   return (
     <li
       key={todo.id}
       className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow"
     >
-      <span>{todo.text}</span>
+      {isEditing ? <input type="text" /> : <span>{todo.text}</span>}
+
       <div>
-        <button className="text-green-500 mr-3">edit</button>
+        <button className="text-green-500 mr-3" onClick={handleEdit}>
+          edit
+        </button>
         <button className="text-red-500">delete</button>
       </div>
     </li>
